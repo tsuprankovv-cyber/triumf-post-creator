7af002029885c485ff8caa693845484d378fce78
tsuprankovv-cyber <tsuprankov.v@gmail.com>
2026-03-19 00:13:49 +0800

    Update states.py

--- states.py
+++ states.py
@@ -6,11 +6,6 @@ class PostWorkflow(StatesGroup):
     writing_text = State()
     adding_buttons = State()
 
-# Состояния для пошагового создания кнопки
 class AddButtonSteps(StatesGroup):
     waiting_for_text = State()
     waiting_for_url = State()
-
-# Состояние для быстрого ввода кнопок (опционально, можно использовать и без него)
-class QuickButtonInput(StatesGroup):
-    waiting_for_input = State()State()
