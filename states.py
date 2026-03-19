a8b218f851885603342472101bd491df9d3311a4
tsuprankovv-cyber <tsuprankov.v@gmail.com>
2026-03-19 19:36:35 +0800

    Update states.py

--- states.py
+++ states.py
@@ -6,7 +6,12 @@ class PostWorkflow(StatesGroup):
     writing_text = State()
     adding_buttons = State()
     ai_input = State()
+    selecting_link = State()
 
 class AddButtonSteps(StatesGroup):
+    waiting_for_text = State()
+    waiting_for_url = State()
+
+class AddLinkSteps(StatesGroup):
     waiting_for_text = State()
     waiting_for_url = State()
